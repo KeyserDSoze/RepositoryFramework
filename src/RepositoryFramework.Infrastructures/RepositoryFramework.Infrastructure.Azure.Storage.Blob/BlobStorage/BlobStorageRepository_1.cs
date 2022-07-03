d9207b1a1074e71a4b4533ab2a2c4a386f0b830b
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2022-07-03 19:18:13 +0200

    blob storage 0.10.3

---
+++ src/RepositoryFramework.Infrastructures/RepositoryFramework.Infrastructure.Azure.Storage.Blob/BlobStorage/BlobStorageRepository_1.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Storage.Blobs;
+using System.Linq.Expressions;
+using System.Text.Json;
+
+namespace RepositoryFramework.Infrastructure.Azure.Storage.Blob
+{
+    internal class BlobStorageRepository<T> : BlobStorageRepository<T, string>, IRepositoryPattern<T>, IQueryPattern<T>, ICommandPattern<T>
+    {
+        public BlobStorageRepository(BlobServiceClientFactory clientFactory) : base(clientFactory)
+        {
+        }
+    }
+}
