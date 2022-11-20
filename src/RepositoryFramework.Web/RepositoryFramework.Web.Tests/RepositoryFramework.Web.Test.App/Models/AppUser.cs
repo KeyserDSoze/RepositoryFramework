d18928386fcf523e25b1ee111cb565d6ef6e438b
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2022-11-20 23:33:39 +0100

    c

--- src/RepositoryFramework.Web/RepositoryFramework.Web.Tests/RepositoryFramework.Web.Test.App/Models/AppUser.cs
+++ src/RepositoryFramework.Web/RepositoryFramework.Web.Tests/RepositoryFramework.Web.Test.App/Models/AppUser.cs
@@ -2,9 +2,9 @@
 {
     public sealed class AppUser
     {
-        public required int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
