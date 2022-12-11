a602e1f9ea7692286bb835693699e2d3546094d2
Alessandro Rapiti <alessandro.rapiti@agic.cloud>
2022-12-11 10:44:43 +0100

    x

--- src/RepositoryFramework.Web/RepositoryFramework.Web.Components/Settings/AppSizingSettings.cs
+++ src/RepositoryFramework.Web/RepositoryFramework.Web.Components/Settings/AppSizingSettings.cs
@@ -9,6 +9,8 @@
         public string IconSize { get; set; } = "1rem";
         public string ColumnWidth { get; set; } = "180px";
         public string InputFontSize { get; set; } = "1.0em";
+        public string NavigationFontSize { get; set; } = "1.8em";
+        public string NavigationFontWeight { get; set; } = "bolder";
         public string TextFontFamily { get; set; } = "Source Sans Pro, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol";
     }
 }
