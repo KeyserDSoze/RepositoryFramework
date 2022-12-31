a47b741f10d4d39b978fc69d5bcf1c15a4a116ff
Alessandro Rapiti <alessandro.rapiti@agic.cloud>
2022-12-31 01:40:40 +0100

    x

--- src/RepositoryFramework.Web/RepositoryFramework.Web.Components/Business/PropertyHandler/FurtherProperty.cs
+++ src/RepositoryFramework.Web/RepositoryFramework.Web.Components/Business/PropertyHandler/FurtherProperty.cs
@@ -2,11 +2,6 @@
 
 namespace RepositoryFramework.Web.Components
 {
-    public static class BasePropertyExtensions
-    {
-        public static FurtherProperty GetFurtherProperty(this BaseProperty baseProperty)
-            => baseProperty.GetProperty<FurtherProperty>(Constant.FurtherProperty);
-    }
     public sealed class FurtherProperty
     {
         public string Id { get; }
