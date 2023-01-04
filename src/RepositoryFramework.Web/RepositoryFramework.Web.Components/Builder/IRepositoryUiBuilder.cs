f8a85c03a8323b561248a3a86dfeafd04e6ec8d3
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-01-04 19:44:28 +0100

    Repository builder

---
+++ src/RepositoryFramework.Web/RepositoryFramework.Web.Components/Builder/IRepositoryUiBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RepositoryFramework.Web.Components.Builder
+{
+    public interface IRepositoryUiBuilder
+    {
+        IServiceCollection Services { get; }
+        IRepositoryUiBuilder AddSkinForUi(string name, Action<AppPalette> settings);
+        IRepositoryUiBuilder AddDefaultSkinForUi();
+        IRepositoryUiBuilder WithAuthenticatedUi();
+    }
+}
--- src/RepositoryFramework.Web/RepositoryFramework.Web.Components/Extensions/ServiceCollectionExtensions.cs
+++ src/RepositoryFramework.Web/RepositoryFramework.Web.Components/Extensions/ServiceCollectionExtensions.cs
@@ -1,13 +1,14 @@
 ﻿using System.Reflection;
 using Radzen;
 using RepositoryFramework.Web.Components;
+using RepositoryFramework.Web.Components.Builder;
 using RepositoryFramework.Web.Components.Services;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddRepositoryUi(this IServiceCollection services,
+        public static IRepositoryUiBuilder AddRepositoryUi(this IServiceCollection services,
            Action<AppSettings> settings)
         {
             var options = new AppSettings()
@@ -27,9 +28,9 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddScoped<ContextMenuService>();
             services.AddScoped<ICopyService, CopyService>();
             services.AddRazorPages();
-            return services;
+            return new RepositoryUiBuilder(services);
         }
-        public static IServiceCollection AddRepositoryUi<T>(this IServiceCollection services,
+        public static IRepositoryUiBuilder AddRepositoryUi<T>(this IServiceCollection services,
            Action<AppSettings> settings)
         {
             var options = new AppSettings()
@@ -40,46 +41,6 @@ namespace Microsoft.Extensions.DependencyInjection
             settings.Invoke(options);
             return services
                 .AddRepositoryUi(settings);
-        }
-        public static IServiceCollection AddSkinForUi(this IServiceCollection services,
-            string name, Action<AppPalette> settings)
-        {
-            var palette = new AppPalette();
-            settings.Invoke(palette);
-            AppPaletteWrapper.Instance.Skins.Add(name, palette);
-            services.AddSingleton(AppPaletteWrapper.Instance);
-            return services;
-        }
-        public static IServiceCollection AddDefaultSkinForUi(this IServiceCollection services)
-        {
-            services.AddSkinForUi("Light", x => { });
-            services.AddSkinForUi("Dark", x =>
-            {
-                x.Primary = "#375a7f";
-                x.Secondary = "#626262";
-                x.Success = "#00bc8c";
-                x.Info = "#17a2b8";
-                x.Warning = "#f39c12";
-                x.Danger = "#e74c3c";
-                x.Light = "#3b3b3b";
-                x.Dark = "#9e9e9e";
-                x.BackgroundColor = "#222";
-                x.Color = "#e1e1e1";
-                x.Table.Color = "#e1e1e1";
-                x.Table.Background = "#222";
-                x.Table.StripedColor = "#d1d1d1";
-                x.Table.StripedBackground = "#333";
-                x.Link.Color = "#eee";
-                x.Link.Hover = "#bbb";
-                x.Button.Color = "#ff6d41";
-                x.Button.Background = "#35a0d7";
-            });
-            return services;
-        }
-        public static IServiceCollection WithAuthenticatedUi(this IServiceCollection services)
-        {
-            AppInternalSettings.Instance.IsAuthenticated = true;
-            return services;
         }
     }
 }
